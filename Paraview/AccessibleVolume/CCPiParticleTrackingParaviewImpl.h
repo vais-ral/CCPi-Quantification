7b794687c9c369aa9b51d291ae0f3d3ac4f8e0ca
Mr. Srikanth Nagella <srikanth.nagella@stfc.ac.uk>
2014-06-18 16:13:38 +0000

    [#1035] barebone for particle tracking in paraview
    git-svn-id: https://ccpforge.cse.rl.ac.uk/svn/iqa/trunk@100 1f895c63-7dff-42ab-8404-156523f462c5

---
+++ Paraview/AccessibleVolume/CCPiParticleTrackingParaviewImpl.h
@@ -0,0 +1,39 @@
+/**
+ * CCPi Paraview plugin of particle tracking 
+ * The implementation of algorithm is in the CCPi core folder
+ *
+ * Author : Mr. Srikanth Nagella
+ * Date   : 06.06.2014
+ */
+
+
+#ifndef CCPIPARTICLETRACKINGPARAVIEWIMPL_H
+#define CCPIPARTICLETRACKINGPARAVIEWIMPL_H
+
+#include "vtkImageAlgorithm.h"
+#include <string>
+#include <vector>
+
+class VTK_EXPORT CCPiParticleTrackingParaviewImpl : public vtkImageAlgorithm 
+{
+public:
+  static CCPiParticleTrackingParaviewImpl* New();
+  vtkTypeMacro(CCPiParticleTrackingParaviewImpl, vtkImageAlgorithm);
+  void PrintSelf(ostream& os, vtkIndent indent);
+
+protected:
+  CCPiParticleTrackingParaviewImpl();
+  ~CCPiParticleTrackingParaviewImpl();
+
+  int RequestData(vtkInformation *, vtkInformationVector **,
+                  vtkInformationVector *);
+
+  int FillInputPortInformation(int port, vtkInformation* info);
+  int FillOutputPortInformation(int port, vtkInformation* info);
+
+private:
+  CCPiParticleTrackingParaviewImpl(const CCPiParticleTrackingParaviewImpl&);  // Not implemented.
+  void operator=(const CCPiParticleTrackingParaviewImpl&);  // Not implemented.
+};
+
+#endif
