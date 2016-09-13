f3cfaaf68c57d27752a0328dcf323c1a903eb4c4
Mr. Srikanth Nagella <srikanth.nagella@stfc.ac.uk>
2016-09-13 14:38:38 +0000

    [#1630] Made the code compatible with VS 2013
    git-svn-id: https://ccpforge.cse.rl.ac.uk/svn/iqa/trunk@172 1f895c63-7dff-42ab-8404-156523f462c5

--- Core/CCPiDefines.h
+++ Core/CCPiDefines.h
@@ -4,8 +4,10 @@
 #if defined(_WIN32) || defined(__WIN32__)
   #if defined(CCPiCore_EXPORTS) || defined(CCPiNexusWidget_EXPORTS) // add by CMake 
     #define  CCPI_EXPORT __declspec(dllexport)
+    #define EXPIMP_TEMPLATE
   #else
     #define  CCPI_EXPORT __declspec(dllimport)
+    #define EXPIMP_TEMPLATE extern
   #endif /* CCPi_EXPORTS */
 #elif defined(linux) || defined(__linux) || defined(__APPLE__)
  #define CCPI_EXPORT
