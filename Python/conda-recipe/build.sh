2a6ad1c242462f398075db80090cb4fd73971015
Srikanth Nagella <srikanth.nagella@stfc.ac.uk>
2017-05-26 10:22:46 +0100

    Fixed #10 modified recipe for linux

--- Python/conda-recipe/build.sh
+++ Python/conda-recipe/build.sh
@@ -1,7 +1,7 @@
-cp -r ${RECIPE_DIR}/.. ${SRC_DIR}/ccpi
-cp -r ${RECIPE_DIR}/../../src/* ${SRC_DIR}/ccpi
-cp ${RECIPE_DIR}/../../src/Algorithms/* ${SRC_DIR}/ccpi
-cp ${RECIPE_DIR}/../../src/Readers/* ${SRC_DIR}/ccpi
+mkdir "%SRC_DIR%\ccpi"
+cp -r "%RECIPE_DIR%\..\.." "%SRC_DIR%\ccpi"
 
-cd ${SRC_DIR}/ccpi
-$PYTHON setup.py install
+cd %SRC_DIR%\ccpi\Python
+
+%PYTHON% setup.py build_ext
+%PYTHON% setup.py install
