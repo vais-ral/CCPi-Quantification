707371796a4ba697df7ef754ef367c0c779425ce
Srikanth Nagella <srikanth.nagella@stfc.ac.uk>
2017-05-26 10:32:15 +0100

    Fixed #10 linux build recipe

--- Python/conda-recipe/build.sh
+++ Python/conda-recipe/build.sh
@@ -1,7 +1,7 @@
-mkdir "%SRC_DIR%/ccpi"
-cp -r "%RECIPE_DIR%/../.." "%SRC_DIR%/ccpi"
+mkdir "$SRC_DIR/ccpi"
+cp -r "$RECIPE_DIR/../.." "$SRC_DIR/ccpi"
 
-cd %SRC_DIR%/ccpi/Python
+cd $SRC_DIR/ccpi/Python
 
-%PYTHON% setup.py build_ext
-%PYTHON% setup.py install
+$PYTHON setup.py build_ext
+$PYTHON setup.py install
