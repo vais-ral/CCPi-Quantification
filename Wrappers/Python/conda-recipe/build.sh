7117b33bd827d8c966632efae040dcf2fc8302bb
Srikanth Nagella <srikanth.nagella@stfc.ac.uk>
2017-11-06 17:11:51 +0000

    Modified python build

--- Wrappers/Python/conda-recipe/build.sh
+++ Wrappers/Python/conda-recipe/build.sh
@@ -3,9 +3,9 @@ if [ -z "$CIL_VERSION" ]; then
     exit 1
 fi  
 mkdir "$SRC_DIR/ccpi"
-cp -r "$RECIPE_DIR/../.." "$SRC_DIR/ccpi"
+cp -r "$RECIPE_DIR/../../../" "$SRC_DIR/ccpi"
 
-cd $SRC_DIR/ccpi/Python
+cd $SRC_DIR/ccpi/Wrappers/Python
 
 $PYTHON setup.py build_ext
 $PYTHON setup.py install
