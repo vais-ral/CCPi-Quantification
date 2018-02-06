32973964029f366320d0811eda436ba4478e5c00
Srikanth Nagella <srikanth.nagella@stfc.ac.uk>
2018-02-06 22:35:57 +0000

    linux fix for new code build

--- recipes/library/build.sh
+++ recipes/library/build.sh
@@ -4,7 +4,7 @@ mkdir ${SRC_DIR}/build
 cp -rv ${RECIPE_DIR}/../../Core/ ${SRC_DIR}/build
 mkdir ${SRC_DIR}/build/build
 cd ${SRC_DIR}/build/build
-cmake -G "Unix Makefiles" -DBUILD_SHARED_LIBS=ON -DCMAKE_BUILD_TYPE=Release -DINSTALL_LIB_DIR="%PREFIX%\Library\lib" -DLIBRARY_LIB="${CONDA_PREFIX}/lib" -DLIBRARY_INC="${CONDA_PREFIX}" -DCMAKE_INSTALL_PREFIX="${PREFIX}" ../Core
+cmake -G "Unix Makefiles" -DBUILD_SHARED_LIBS=ON -DCMAKE_BUILD_TYPE=Release -DINSTALL_LIB_DIR="${CONDA_PREFIX}/lib" -DLIBRARY_LIB="${CONDA_PREFIX}/lib" -DLIBRARY_INC="${CONDA_PREFIX}" -DCMAKE_INSTALL_PREFIX="${PREFIX}" ../Core
 
 make -j2 VERBOSE=1
 make install
